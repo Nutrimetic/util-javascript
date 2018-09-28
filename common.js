21016124abba48fd947ee953c3a1a909d106972b
U-TMLG\BCH17450 <BCH17450@TOU1DSK0081.teamlog.intra>
2018-09-28 17:19:11 +0200

    setting example on common.js for reusability

--- common.js
+++ common.js
@@ -32,3 +32,83 @@ function getCookie(name) {
 	}
 	return cookieValue;
 }
+
+/*
+ * Exemple d'utilisation
+ * 
+ */
+var Conversation = {
+		
+		acquereur : "",
+		purchaser : "",
+		listeMessage : [],
+		dateDernierMessage : "",
+		
+		idChat : "",
+		
+		init: function(acquereur, purchaser, listeMessage, dateDernierMessage, idChat) {
+			this.acquereur = acquereur;
+			this.purchaser = purchaser;
+			this.listeMessage = listeMessage;
+			this.dateDernierMessage = dateDernierMessage;
+			this.idChat = idChat;
+		},
+
+		toString: function() {
+			return "acquereur : " + acquereur + "purchaser : " + purchaser + "listeMessage : " + listeMessage.toString();
+		},
+
+		envoyerDonnees : function(idMessagerie) {
+			var quotationRequest = getConnectionAwareXmlHttpRequest('POST', 'messageChat', function(ResponseText) {
+				
+			})
+			quotationRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			quotationRequest.setRequestHeader("Accept", "application/json");
+			
+			var jsonData = {
+				'acquereur'	: this.acquereur,
+				'message' : $("#" + idMessagerie).val()
+			}
+			jsonData = JSON.stringify(jsonData);
+		    quotationRequest.send(jsonData);
+		    
+			$("#" + idMessagerie).val("");
+		},
+
+		rafraichir : function(idChat) {
+			var theChat = this;
+			var quotationRequest = getConnectionAwareXmlHttpRequest('POST', 'messageChats', function(ResponseText) {
+				//2 cas de figure possible : le tableau est vide ou il ne l'est pas
+				if(theChat.listeMessage == null) {
+					theChat.listeMessage = [];
+				}
+				theChat.listeMessage = theChat.listeMessage.concat(JSON.parse(ResponseText));
+				
+				//on vide la liste des messages
+				//clearContent("msg-conversation-content");
+				
+				//on affiche la liste des message à l'écran et on met à jour la date
+				for(var newMessageIndex=0; newMessageIndex < theChat.listeMessage.length; newMessageIndex++) {
+					if((theChat.dateDernierMessage == null) || (theChat.dateDernierMessage < theChat.listeMessage[newMessageIndex].date)){
+						theChat.dateDernierMessage = theChat.listeMessage[newMessageIndex].date;
+						
+						//TODO on call la méthode de création d'un message RenderNew
+						var contactListContainer = document.getElementById('msg-conversation-content');
+						contactListContainer.appendChild(renderWaitingMessagesItem(newMessageIndex, theChat.listeMessage[newMessageIndex].purchaser, theChat.listeMessage[newMessageIndex].date, theChat.listeMessage[newMessageIndex].message));
+					}
+					
+					
+					
+					
+				}
+			})
+			quotationRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+			quotationRequest.setRequestHeader("Accept", "application/json");
+			var jsonData = {
+				'date' : this.dateDernierMessage,
+				'acquereur'	: this.acquereur
+			}
+			jsonData = JSON.stringify(jsonData);
+			quotationRequest.send(jsonData);
+		},
+	}*/
